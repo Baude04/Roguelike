3bf4a48eb36afe2aab9b820c6c5b6aeb560905b8
Baude04 <baudouindelavillebauge18@gmail.com>
2019-11-06 16:56:44 +0100

    Add hero game object
    -add character movement script
    -add tile walk script
    -so the hero can now move when you click to adjacent tile which have the script attached to it

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite upSprite;
+    [SerializeField]
+    private Sprite downSprite;
+    [SerializeField]
+    private Sprite leftSprite;
+    [SerializeField]
+    private Sprite rightSprite;
+
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void Move(Vector2Int direction)
+    {
+        Sprite newSprite = downSprite;      //default value
+        if (direction == Vector2Int.up) newSprite = upSprite;
+        else if (direction == Vector2Int.down) newSprite = downSprite;
+        else if (direction == Vector2Int.left) newSprite = leftSprite;
+        else if (direction == Vector2Int.right) newSprite = rightSprite;
+        else Debug.LogError("Unexpected value");
+        spriteRenderer.sprite = newSprite;
+
+        transform.Translate(new Vector3(direction.x, direction.y));
+    }
+    /*private void OnMouseDown()
+    //test
+    {
+        Move(Vector2Int.right);
+    }*/
+}
---
+++ Assets/Scripts/WalkOnTileScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkOnTileScript : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        
+        if (Vector3.Distance(transform.position, player.transform.position) <= 1f)
+        {
+            Vector3 direction3d = transform.position - player.transform.position;
+            Vector2Int direction2d = new Vector2Int((int)direction3d.x, (int)direction3d.y);
+            player.GetComponent<CharacterMovementScript>().Move(direction2d);
+        }
+    }
+}
